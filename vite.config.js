964f55dc7ca6a65e2d6c2fe6ae2f9701d84eaec6
Juan Hernandez <camilohdez1017@gmail.com>
2025-08-01 16:01:03 -0500

    vite.config added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/js-vite-blackjack-juanito/',
   server: {
     port: 3000, // Cambia este número por el puerto que quieras
   },
